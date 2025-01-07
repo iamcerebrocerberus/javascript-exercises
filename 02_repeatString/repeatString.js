6ebe4364797ca2f99ac99564a58bdad594f58ad2
iamcerebrocerberus <sethamekmen@gmail.com>
2025-01-07 21:03:00 +0000

    feat: implement functionality for 02/

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, repeat) {
+  let repeatStr = "";
+  if (repeat < 0) return "ERROR";
+  for (let i = 0; i < repeat; i++) {
+    repeatStr += str;
+  }
+  return repeatStr;
 };
 
 // Do not edit below this line
